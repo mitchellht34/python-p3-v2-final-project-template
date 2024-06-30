91cd198918ec201e62172ac66da0f7052d828ae8
Mitch Toran <mitchellht34@gmail.com>
2024-06-30 17:47:15 -0400

    add database reset and seeds to debug file

--- lib/debug.py
+++ lib/debug.py
@@ -6,5 +6,17 @@ from models.recipient import Recipient
 from models.gift import Gift
 import ipdb
 
+def reset_database():
+    Recipient.drop_table()
+    # Gift.drop_table()
+    Recipient.create_table()
+    # Gift.create_table()
 
+    # Create seed data
+    mom = Recipient.create("Mom")
+    dad = Recipient.create("Dad")
+    zach = Recipient.create("Zach")
+    logan = Recipient.create("Logan")
+
+reset_database()
 ipdb.set_trace()
